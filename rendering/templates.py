280ffa7841377c5d61676ad597959c7a9c2f8199
Macros Meng <Meng-yc@outlook.com>
2026-07-12 16:23:18 +0800

    docs: 添加部分模板内容

---
+++ rendering/templates.py
@@ -0,0 +1,150 @@
+home = """\
+<div align="center">
+
+<img width="156" src="./images/aiwb-icon.png">
+
+<h1>
+<picture>
+      <source media="(prefers-color-scheme: dark)" srcset="./images/fontlogo/aiwb-font-white.png">
+      <img alt="aiwb-logo" src="./images/fontlogo/aiwb-font-dark.png" height="28">
+    </picture>
+</h1>
+
+专为广大中小学电教打造的班级希沃/鸿合等<br/>一体机/数字白板/班班通一站式软件推荐清单<br/>和实用知识手册，助你在新学期快速上手<br/>班级一体机新玩法！
+<br/>**为广大电教倾情撰写，让班级大屏更好用！**
+
+**🌟 风带来故事的种子，时间使之发芽 🌟**
+
+<a href="./CONTRIBUTING.md">
+    <picture>
+      <source media="(prefers-color-scheme: dark)" srcset="./images/navigation/贡献指南-白.svg">
+      <img alt="贡献指南" src="./images/navigation/贡献指南-黑.svg" height="52">
+    </picture>
+</a>&nbsp;
+<a href="https://github.com/awesome-iwb/awesome-iwb/issues/57">
+    <picture>
+      <source media="(prefers-color-scheme: dark)" srcset="./images/navigation/最新项目-白.svg">
+      <img alt="最新项目" src="./images/navigation/最新项目-黑.svg" height="52">
+    </picture>
+</a>&nbsp;
+<img alt="分隔符" height="52" src="./images/navigation/分隔符.svg">&nbsp;
+<a href="#%EF%B8%8F-屏幕批注与白板软件">
+    <picture>
+      <source media="(prefers-color-scheme: dark)" srcset="./images/navigation/白板软件-白.svg">
+      <img alt="白板软件" src="./images/navigation/白板软件-黑.svg" height="52">
+    </picture>
+</a>&nbsp;
+<a href="#-课表与看板类软件">
+    <picture>
+      <source media="(prefers-color-scheme: dark)" srcset="./images/navigation/课表看板-白.svg">
+      <img alt="课表看板" src="./images/navigation/课表看板-黑.svg" height="52">
+    </picture>
+</a>&nbsp;
+<a href="#%EF%B8%8F-辅助类软件与实用工具">
+    <picture>
+      <source media="(prefers-color-scheme: dark)" srcset="./images/navigation/工具辅助-白.svg">
+      <img alt="工具辅助" src="./images/navigation/工具辅助-黑.svg" height="52">
+    </picture>
+</a>&nbsp;
+
+<br/>
+<br/>
+
+[![Awesome](https://cdn.rawgit.com/sindresorhus/awesome/d7305f38d29fed78fa85652e3a63e154dd8e8829/media/badge.svg)](https://github.com/sindresorhus/awesome)
+[![CC BY-NC-SA 4.0](https://img.shields.io/badge/License-CC_BY--NC--SA_4.0-blue.svg)](https://creativecommons.org/licenses/by-nc-sa/4.0)
+![Maintenance](https://img.shields.io/badge/Maintained%3F-YES-green.svg)<br/>
+![GitHub last commit](https://img.shields.io/github/last-commit/awesome-iwb/awesome-iwb)
+![Visitors](https://api.visitorbadge.io/api/combined?path=https://github.com/awesome-iwb/awesome-iwb&label=Visitors&countColor=%23263759&style=flat)
+![Lastest Update](https://img.shields.io/badge/%E6%9C%80%E5%90%8E%E6%9B%B4%E6%96%B0-2026%E5%B9%B46%E6%9C%887%E6%97%A5-orange)
+[![交流群](https://img.shields.io/badge/-%E4%BA%A4%E6%B5%81%E7%BE%A4%20995186133-white?style=flat&logo=qq)](https://qm.qq.com/q/gro00q2v8A)
+![Repobeats Analyzer](https://repobeats.axiom.co/api/embed/e21726d2782bb081ef5d8f89360f1ed0d93af4ef.svg "Repobeats analytics image")
+
+</div>
+
+> [!note]
+> 诚邀各位开发者加入 Awesome Iwb 的交流群。如果你的项目在 Awesome Iwb 上面上榜了，请加入开发者交流群：995186133。
+
+> [!note]
+> Awesome Iwb 官网已推出，关于本readme的问题将不再受理，后续将会使用自动化工具生成新版readme，敬请期待！如需提交项目请登录我们的官网：https://aiwb.smart-teach.cn
+
+---
+
+## ⚠️ 注意
+
+> [!IMPORTANT]
+> 在使用任何软件之前，应仔细阅读其仓库中的 README 或其他形式的说明。本文档旨在推荐以下优质软件，尽可能详细展现其功能，如欲提出问题，请前往对应开源仓库/交流平台。
+
+> [!WARNING]
+> 以下软件均为免费或非买断制，如遇收费提示请注意甄别是否为盗版软件！
+
+## 速览
+
+[![Quicklook Page goto](./rendering/images/quicklook.png)](./docs/quicklook.md)
+
+## ✏️ 屏幕批注与白板软件
+
+[![notation_and_whiteboard goto](./rendering/images/notation_and_whiteboard.png)](./docs/notation_and_whiteboard.md)
+
+## 🗓️ 课表与看板类软件
+
+[![timetable_and_dashboard goto](./rendering/images/timetable_and_dashboard.png)](./docs/timetable_and_dashboard.md)
+
+## 🛠️ 辅助类软件与实用工具
+
+[![timetable_and_dashboard goto](./rendering/images/timetable_and_dashboard.png)](./docs/timetable_and_dashboard.md)
+
+
+## 🌟 Stars 历史图表
+
+<div align="center">
+
+[![Star 历史](https://starchart.cc/Awesome-Iwb/Awesome-Iwb.svg?variant=adaptive)](https://starchart.cc/Awesome-Iwb/Awesome-Iwb/stargazers)
+
+**如果这个项目对您有帮助，请点亮 Star ⭐！**
+
+</div>
+
+
+## 📄 协议
+
+<p xmlns:cc="http://creativecommons.org/ns#" >本作品已获得 <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/?ref=chooser-v1" target="_blank" rel="license noopener noreferrer" style="display:inline-block;">CC BY-NC-SA 4.0 许可<img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/cc.svg?ref=chooser-v1" alt=""><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/by.svg?ref=chooser-v1" alt=""><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/nc.svg?ref=chooser-v1" alt=""><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/sa.svg?ref=chooser-v1" alt=""></a></p>
+
+## 💗 友情链接
+
+- [iNKORE!](https://www.inkore.net/) - iNKORE! 官方网站
+
+  本集合收录的部分项目使用了 iNKORE.UI.WPF.Modern 作为其 UI 库。
+  <image src="https://github.com/iNKORE-NET/UI.WPF.Modern/blob/main/assets/images/badges/UI.WPF.Modern_Main_Button.svg?raw=true" alt="iNKORE.UI.WPF.Modern"/>
+
+- [PhosoftWebPages](https://xiapo.top/) - 溪夏坡Zpcin 的个人网站
+89	+
+- [Awesome-Class-Softwares](https://acs.jursin.top/) - Awesome-Class-Softwares 网站
+3990	+
+- [Jursin's Blog](https://blog.jursin.top/) - Jursin 的博客
+- [宏星工作室｜MacroSTAR](https://macrostar.top/) - 宏星工作室官方网站，满天星光，终将汇聚宏伟星河！
+- [电教委员指南](https://cnel.smart-teach.cn/) - 做一名优秀的电教委员
+
+## 🔗 相关链接
+
+- <image src="https://github.com/ClassIsland.png" height="18"/> **[ClassIsland Hub](https://hub.classisland.tech/)** - 展示亿些 ClassIsland 交流群/频道里面的乐子。
+
+- **[RinlitHub](https://mcableblank.github.io/RinlitHub/)** - Class-Widgets 开发者的Hub，收集 CW 交流群里面的乐子。
+
+- <image src="./images/misaka-space-icon.png" height="18"/> **[电教委入门指南](https://tutorial.misaka.space/)** - 从小白到高手，轻松玩转班级一体机。
+
+- <image src="./images/misaka-space-icon.png" height="18"/> **[seewoGeekWiki](https://wiki.misaka.space/)** - 希沃业绩售后冲击部官方 Wiki。
+
+- **[希沃折腾手册](https://www.kdocs.cn/l/coHT3CUcoQDV)** - 希沃一体机折腾手册，整理了许多一体机折腾的教程。
+
+
+## 👋 同类项目推荐
+
+
+[![Jursin/Awesome-Class-Softwares](https://github-readme-stats-indol-phi-94.vercel.app/api/pin/?username=Jursin&repo=Awesome-Class-Softwares&show_owner=true)](https://github.com/Jursin/Awesome-Class-Softwares)
+
+## Copyright
+
+© Copyright 2025-2026 智教联盟SmartTeachCN
+
+
+"""
